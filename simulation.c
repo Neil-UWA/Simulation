1a5f42948c2ac31dd34ba0f6f622f3609d2bf05d
Neil <lvjinang@yahoo.com.au>
2012-11-14 22:32:45 +0800

    Two modules, AP and Mobile Client

--- simulation.c
+++ simulation.c
@@ -1,24 +1,60 @@
 #include <cnet.h>
 #include <time.h>
+#include <string.h>
 #include "mapping.h"
 #include "walking.h"
 
+#define	EV_TALKING	EV_TIMER1
+#define EV_BEACON	EV_TIMER2
+
+#define MAX_SIZE 50
+//static char message[] = "hello";
+static char beacon[] = "I am a beacon frame";
+
+typedef struct{
+	char	message[MAX_SIZE];
+	//int		nodetype; //TYPE OF THE NODE
+	int		nodenumber;
+}package;
+
 EVENT_HANDLER(click){
-    time_t  now;
-    time(&now);
-    printf("hello\n");    
-    printf("%s\n", ctime(&now));
+  time_t  now;
+  time(&now);
+  printf("hello\n");    
+  printf("%s\n", ctime(&now));
+  CNET_start_timer(EV_TALKING, (CnetTime)2000000, 0);
+}
+
+EVENT_HANDLER(ap){
+  static package frame;
+  size_t	len = sizeof(frame);
+
+	frame.nodenumber = nodeinfo.nodenumber;
+	  strcpy(frame.message,beacon);
+  printf("I am an accesspoint with one wlan link\n");
+
+  CNET_write_physical_reliable(1,&frame,&len);
+  CNET_start_timer(EV_BEACON, (CnetTime)2000000, 0);
 }
 
 EVENT_HANDLER(reboot_node){
-	char**	argv = (char**) data;
-	if(argv[0])
-	{
-		readmap(argv[0]);
-		init_walking();
-		start_walking();
-		
-		CNET_set_handler(EV_DEBUG1, click, 0);
-		CNET_set_debug_string(EV_DEBUG1, "click me");
-	}
+  char**	argv = (char**) data;
+
+  if(argv[0]){
+    readmap(argv[0]);
+	
+    //FUNCTIONS FOR ACCESS POINTS
+    if(nodeinfo.nodetype == NT_ACCESSPOINT) {
+      CNET_set_handler(EV_BEACON, ap, 0);
+      CNET_start_timer(EV_BEACON, (CnetTime)1000000, 0);
+    }
+
+    //FUNCTIONS FOR MOBILE CLIENTS
+    if(nodeinfo.nodetype == NT_MOBILE) {
+	  init_walking();
+	  start_walking();
+      CNET_set_handler(EV_TALKING, click, 0);
+      CNET_start_timer(EV_TALKING, (CnetTime)1000000, 0);
+    }
+  }
 }
